3dd993ad8eb6dc0325e018b637e575e67914d0a3
yanfeng-li <60999693+yanfeng-li@users.noreply.github.com>
2022-05-11 09:43:58 +0800

    update mkdocs nav translation (#49)

--- tests/unittest/test_docs/test_mkdocs.py
+++ tests/unittest/test_docs/test_mkdocs.py
@@ -2,12 +2,12 @@ from .docs import MkdocsParse
 
 
 def test_mkdocs():
-    nav_translations_mismatch, nav_mismatch = MkdocsParse().validate_path()
-    message = (
-        "Mkdocs nav_translations title not match: " + f"{nav_translations_mismatch}.\n"
-        if nav_translations_mismatch
-        else ""
-    )
-    message += f"Mkdocs nav path not match: {nav_mismatch}" if nav_mismatch else ""
-    if message:
-        assert False, message
+    nav_mismatch = MkdocsParse().validate_path()
+    if len(nav_mismatch):
+        assert False, f"Mkdocs nav path not match: {nav_mismatch}"
+
+
+def test_title():
+    nav_mismatch = MkdocsParse().validate_title()
+    if len(nav_mismatch):
+        assert False, f"Mkdocs nav_translations not match: {nav_mismatch}"
