3251c56c3dd8369d0521852f1830fcc7fff8bbb1
wjiuhe <wjiuhe@gmail.com>
2022-05-22 20:08:28 +0800

    Remove Default Prediction API Function

--- pinferencia/apis/default/v1/routers/__init__.py
+++ pinferencia/apis/default/v1/routers/__init__.py
@@ -4,11 +4,8 @@ from .basic import router as basic_router
 from .management import router as management_router
 from .metadata import router as metadata_router
 
-# from .predict import router as predict_router
-
 router = APIRouter()
 
 router.include_router(basic_router)
 router.include_router(metadata_router)
 router.include_router(management_router)
-# router.include_router(predict_router)
